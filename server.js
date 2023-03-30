4060e73c6e4894039133a5a07d912644fd2adcf4
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-03-30 13:56:48 +0500

    Creating an express server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Welcome");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
