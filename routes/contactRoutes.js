af0b0e0c9a53180ea8d5da2f02d0ad2e7c8bc0fd
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-03-30 15:46:44 +0500

    Optimizing routes structure

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,14 +8,12 @@ const {
   deleteContact,
 } = require("../controllers/contactController");
 
-router.route("/").get(getContact);
+router.route("/").get(getContact).post(createContact);
 
-router.route("/:id").get(getContactById);
-
-router.route("/").post(createContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router
+  .route("/:id")
+  .get(getContactById)
+  .put(updateContact)
+  .delete(deleteContact);
 
 module.exports = router;
