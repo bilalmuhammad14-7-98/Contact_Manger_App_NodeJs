4e7b124b28b930dcbcd3523f87537b250f921cf8
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-04-06 15:59:05 +0500

    CRUD (Update Contact)

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -51,9 +51,19 @@ const createContact = asyncHandler(async (req, res) => {
 // access public
 
 const updateContact = asyncHandler(async (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Contacts Updated Successfully for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    console.log("no contact");
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
 });
 
 // @desc Delete Contact
